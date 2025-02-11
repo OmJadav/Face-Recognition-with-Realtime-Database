25c1824b8b88c175d1810c713d97cc1620d222ee
Om Jadav <omjadav04@gmail.com>
2025-02-12 01:25:40 +0530

    final submit

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -5,14 +5,12 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
     'databaseURL':"https://faceattendance-a4fe5-default-rtdb.firebaseio.com/"
 })
-
 ref=db.reference('Students')
-
 data={
-    "312654":
+    "321654":
         {
             "name":"Mukesh Ambani",
-            "branch":"Chuno lagao",
+            "branch":"MBA",
             "roll":312654,
             "batch":2021,
             "total_attendance":10,
@@ -20,24 +18,23 @@ data={
         },
     "852741":
         {
-            "name": "Mark Zuckerburg",
-            "branch":"Time pass",
+            "name": "Bill Gates",
+            "branch":"CSE",
             "roll":852741,
             "batch": 2021,
-            "total_attendance": 12,
+            "total_attendance": 10,
             "last_attendance_time": "2025-02-11 08:34:23"
         },
     "963852":
         {
             "name": "Elon Musk",
-            "branch": "Time pass",
+            "branch": "CSE",
             "roll": 963852,
             "batch": 2021,
-            "total_attendance": 5,
+            "total_attendance": 10,
             "last_attendance_time": "2025-02-11 08:49:23"
         }
 
 }
-
 for key,value in data.items():
     ref.child(key).set(value)
